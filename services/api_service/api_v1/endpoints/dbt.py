2ec659c926217ad08cde90ea0bd717c51b2e6298
datnguye <datnguyen.it09@gmail.com>
2021-10-03 10:12:51 +0700

    Remove failed chick

--- services/api_service/api_v1/endpoints/dbt.py
+++ services/api_service/api_v1/endpoints/dbt.py
@@ -19,7 +19,7 @@ def dbt_run(
         result = instance.execute("Provisioning flow", [
             DBT(
                 action=DbtAction.SEED,
-                target="failed",
+                target=DBT_TARGET,
                 project_dir=DBT_PROJECT_DIR,
                 full_refresh=True
             ),
